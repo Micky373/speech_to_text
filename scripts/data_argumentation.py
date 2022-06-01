25e3b7d4ff622a8beaae0a25583189f00365d02e
abu-bakarr <abangura14@yahoo.com>
2022-06-01 08:00:58 +0000

    Add plot function for data augmentation

--- scripts/data_argumentation.py
+++ scripts/data_argumentation.py
@@ -22,3 +22,15 @@ def change_speed(data, speed_factor):
 def change_pitch(data, sampling_rate):
     n_steps = np.random.randint(-1, 2)
     return librosa.effects.pitch_shift(data, sampling_rate, n_steps)
+
+
+def plot_spec(data: np.array, sr: int) -> None:
+    '''
+    Function for plotting spectrogram along with amplitude wave graph
+    '''
+
+    fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+    ax[0].title.set_text(f'Shfiting the wave by Times {sr/10}')
+    ax[0].specgram(data, Fs=2)
+    ax[1].set_ylabel('Amplitude')
+    ax[1].plot(np.linspace(0, 1, len(data)), data)
