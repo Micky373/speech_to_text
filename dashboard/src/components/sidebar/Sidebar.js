5167483e7142c27abc57a2824f04d9f1a5772792
Jeremy Tesh <jermitesh20@gmail.com>
2022-06-06 09:18:02 +0300

    feat:flask api config

--- dashboard/src/components/sidebar/Sidebar.js
+++ dashboard/src/components/sidebar/Sidebar.js
@@ -11,9 +11,9 @@ import { Link, NavLink } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div className="h-screen w-[300px]">
-      <div className="bg-[#333] h-full text-white">
-        <div className="text-center p-9 font-serif text-2xl">
-          <h1> AMHARIC SPEECH TO TEXT</h1>
+      <div className="bg-[#333] h-full text-white font-bold">
+        <div className="text-center pt-9 pb-9 font-serif text-2xl">
+          <h1 className="p-2"> AMHARIC SPEECH TO TEXT</h1>
         </div>
         <hr></hr>
         <div className="flex flex-col w-full h-full pt-28 pl-8 text-xl">
--- dashboard/src/pages/Upload.js
+++ dashboard/src/pages/Upload.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Upload() {
+  useEffect(() => {
+    fetch("/output").then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+      })
+    );
+  }, []);
+
   return (
     <div className="w-full h-full pt-10">
       <h1 className="text-center">Upload your sound file here</h1>
       <div className="flex flex-col h-full  items-center pt-10">
-        <div className="border-5 w-[300px] mt-10 pt-10"></div>
-        <div className="border-5 w-[300px] mt-10 pt-10"></div>
+        <div className="border-5 w-[700px] mt-10 p-2">
+          <h1 className="text-center">Upload file</h1>
+        </div>
+        <div className="border-5 w-[700px] h-[700px] mt-10 pt-10"></div>
       </div>
     </div>
   );
