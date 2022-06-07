784158df8d426907388de32d0412c724cd3df4ec
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-06-06 17:17:20 -0700

    missing wav file error fixed

---
+++ scripts/acoustic_modeling.py
@@ -0,0 +1,529 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+from IPython import display
+from jiwer import wer
+import librosa
+import numpy as np
+import soundfile
+import scipy.io.wavfile as wav
+
+import random
+from python_speech_features import mfcc
+import librosa
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from keras import backend as K
+from keras.models import Model
+from keras.layers import (BatchNormalization, Conv1D, Dense, Input, 
+    TimeDistributed, Activation, Bidirectional, SimpleRNN, GRU, LSTM)
+from keras.utils.vis_utils import plot_model
+
+
+import _pickle as pickle
+from numpy.lib.stride_tricks import as_strided
+
+from keras.layers import (Input, Lambda)
+from tensorflow.keras.optimizers import SGD
+from keras.callbacks import ModelCheckpoint   
+import os
+
+RNG_SEED = 123
+
+class AudioGenerator():
+    def __init__(self,  train_meta, valid_meta, step=10, window=20, max_freq=8000, mfcc_dim=13,
+        minibatch_size=20):
+        """
+        Params:
+            step (int): Step size in milliseconds between windows (for spectrogram ONLY)
+            window (int): FFT window size in milliseconds (for spectrogram ONLY)
+            max_freq (int): Only FFT bins corresponding to frequencies between
+                [0, max_freq] are returned (for spectrogram ONLY)
+            desc_file (str, optional): Path to a JSON-line file that contains
+                labels and paths to the audio files. If this is None, then
+                load metadata right away
+        """
+        self.feat_dim = calc_feat_dim(window, max_freq)
+        self.mfcc_dim = mfcc_dim
+        self.feats_mean = np.zeros((self.feat_dim,))
+        self.feats_std = np.ones((self.feat_dim,))
+        self.rng = random.Random(RNG_SEED)
+        self.step = step
+        self.window = window
+        self.train_meta = train_meta
+        self.valid_meta = valid_meta
+        self.max_freq = max_freq
+        self.cur_train_index = 0
+        self.cur_valid_index = 0
+        self.cur_test_index = 0
+        self.minibatch_size = minibatch_size
+
+    def get_batch(self, partition):
+        """ Obtain a batch of train, validation, or test data
+        """
+        if partition == 'train':
+            audio_paths = self.train_audio_paths
+            cur_index = self.cur_train_index
+            texts = self.train_texts
+        elif partition == 'valid':
+            audio_paths = self.valid_audio_paths
+            cur_index = self.cur_valid_index
+            texts = self.valid_texts
+        elif partition == 'test':
+            audio_paths = self.test_audio_paths
+            cur_index = self.test_valid_index
+            texts = self.test_texts
+        else:
+            raise Exception("Invalid partition. "
+                "Must be train/validation")
+
+        features = [self.normalize(self.featurize(a)) for a in 
+            audio_paths[cur_index:cur_index+self.minibatch_size]]
+
+        # calculate necessary sizes
+        max_length = max([features[i].shape[0] 
+            for i in range(0, self.minibatch_size)])
+        max_string_length = max([len(texts[cur_index+i]) 
+            for i in range(0, self.minibatch_size)])
+        
+        # initialize the arrays
+        X_data = np.zeros([self.minibatch_size, max_length, 
+            self.feat_dim*(False) + self.mfcc_dim*(True)])
+        labels = np.ones([self.minibatch_size, max_string_length]) * 28
+        input_length = np.zeros([self.minibatch_size, 1])
+        label_length = np.zeros([self.minibatch_size, 1])
+        
+        for i in range(0, self.minibatch_size):
+            # calculate X_data & input_length
+            feat = features[i]
+            input_length[i] = feat.shape[0]
+            X_data[i, :feat.shape[0], :] = feat
+
+            # calculate labels & label_length
+            label = np.array(text_to_int_sequence(texts[cur_index+i])) 
+            labels[i, :len(label)] = label
+            label_length[i] = len(label)
+ 
+        # return the arrays
+        outputs = {'ctc': np.zeros([self.minibatch_size])}
+        inputs = {'the_input': X_data, 
+                  'the_labels': labels, 
+                  'input_length': input_length, 
+                  'label_length': label_length 
+                 }
+        return (inputs, outputs)
+
+    def shuffle_data_by_partition(self, partition):
+        """ Shuffle the training or validation data
+        """
+        if partition == 'train':
+            self.train_audio_paths, self.train_durations, self.train_texts = shuffle_data(
+                self.train_audio_paths, self.train_durations, self.train_texts)
+            self.train_length = len(self.train_texts)
+        elif partition == 'valid':
+            self.valid_audio_paths, self.valid_durations, self.valid_texts = shuffle_data(
+                self.valid_audio_paths, self.valid_durations, self.valid_texts)
+            self.valid_length = len(self.valid_texts)
+        else:
+            raise Exception("Invalid partition. "
+                "Must be train/validation")
+
+    
+    def next_train(self):
+        """ Obtain a batch of training data
+        """
+        while True:
+            ret = self.get_batch('train')
+            self.cur_train_index += self.minibatch_size
+            if self.cur_train_index >= len(self.train_texts) - self.minibatch_size:
+                self.cur_train_index = 0
+                self.shuffle_data_by_partition('train')
+            yield ret    
+
+    def next_valid(self):
+        """ Obtain a batch of validation data
+        """
+        while True:
+            ret = self.get_batch('valid')
+            self.cur_valid_index += self.minibatch_size
+            if self.cur_valid_index >= len(self.valid_texts) - self.minibatch_size:
+                self.cur_valid_index = 0
+                self.shuffle_data_by_partition('valid')
+            yield ret
+
+
+    def load_train_data(self):
+        self.load_metadata_from_desc_file('train')
+        self.fit_train()
+
+    def load_validation_data(self):
+        self.load_metadata_from_desc_file('validation')
+
+    
+    def load_metadata_from_desc_file(self,partition):
+        
+        if partition == 'train':
+            self.train_audio_paths = self.train_meta["Output"].tolist()
+            self.train_durations = self.train_meta["Duration"].tolist()
+            self.train_texts = self.train_meta["Target"].tolist()
+        elif partition == 'validation':
+            self.valid_audio_paths = self.valid_meta["Output"].tolist()
+            self.valid_durations = self.valid_meta["Duration"].tolist()
+            self.valid_texts = self.valid_meta["Target"].tolist()
+        else:
+            raise Exception("Invalid partition to load metadata. "
+             "Must be train/validation/test")
+            
+    def fit_train(self, k_samples=100):
+        """ Estimate the mean and std of the features from the training set
+        Params:
+            k_samples (int): Use this number of samples for estimation
+        """
+        k_samples = min(k_samples, len(self.train_audio_paths))
+        samples = self.rng.sample(self.train_audio_paths, k_samples)
+        feats = [self.featurize(s) for s in samples]
+        feats = np.vstack(feats)
+        self.feats_mean = np.mean(feats, axis=0)
+        self.feats_std = np.std(feats, axis=0)
+        
+    def featurize(self, audio_clip):
+        """ For a given audio clip, calculate the corresponding feature
+        Params:
+            audio_clip (str): Path to the audio clip
+        """
+        (rate, sig) = wav.read(audio_clip)
+        return mfcc(sig, rate, winlen=(self.window*0.001), winstep=(self.step*0.001), nfft=1024, numcep=self.mfcc_dim, )
+
+    def normalize(self, feature, eps=1e-14):
+        """ Center a feature using the mean and std
+        Params:
+            feature (numpy.ndarray): Feature to normalize
+        """
+        return (feature - self.feats_mean) / (self.feats_std + eps)
+
+    def train_length(self):
+        return len(self.train_texts)
+    
+    def valid_length(self):
+        return len(self.valid_texts)
+
+
+def shuffle_data(audio_paths, durations, texts):
+    """ Shuffle the data (called after making a complete pass through 
+        training or validation data during the training process)
+    Params:
+        audio_paths (list): Paths to audio clips
+        durations (list): Durations of utterances for each audio clip
+        texts (list): Sentences uttered in each audio clip
+    """
+    p = np.random.permutation(len(audio_paths))
+    audio_paths = [audio_paths[i] for i in p] 
+    durations = [durations[i] for i in p] 
+    texts = [texts[i] for i in p]
+    return audio_paths, durations, texts
+
+def vis_train_features(index=0):
+    """ Visualizing the data point in the training set at the supplied index
+    """
+    
+    # obtain mfcc
+    audio_gen = AudioGenerator()
+    audio_gen.load_train_data()
+    vis_mfcc_feature = audio_gen.normalize(audio_gen.featurize(vis_audio_path))
+    # obtain text label
+    vis_text = audio_gen.train_texts[index]
+    # obtain raw audio
+    vis_raw_audio, _ = librosa.load(amharic_path(vis_audio_path))
+    # print total number of training examples
+    print('There are %d total training examples.' % len(audio_gen.train_audio_paths))
+    # return labels for plotting
+    return vis_text, vis_raw_audio, vis_mfcc_feature, vis_spectrogram_feature, vis_audio_path
+
+
+def plot_raw_audio(vis_raw_audio, title='Audio Signal', size=(12, 3)):
+    fig = plt.figure(figsize=size)
+    ax = fig.add_subplot(111)
+    steps = len(vis_raw_audio)
+    ax.plot(np.linspace(1, steps, steps), vis_raw_audio)
+    plt.title(title)
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+def plot_mfcc_feature(vis_mfcc_feature):
+    # plot the MFCC feature
+    fig = plt.figure(figsize=(12,5))
+    ax = fig.add_subplot(111)
+    im = ax.imshow(vis_mfcc_feature, cmap=plt.cm.jet, aspect='auto')
+    plt.title('Normalized MFCC')
+    plt.ylabel('Time')
+    plt.xlabel('MFCC Coefficient')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im, cax=cax)
+    ax.set_xticks(np.arange(0, 13, 2), minor=False);
+    plt.show()
+
+def plot_spectrogram_feature(vis_spectrogram_feature):
+    # plot the normalized spectrogram
+    fig = plt.figure(figsize=(12,5))
+    ax = fig.add_subplot(111)
+    im = ax.imshow(vis_spectrogram_feature, cmap=plt.cm.jet, aspect='auto')
+    plt.title('Normalized Spectrogram')
+    plt.ylabel('Time')
+    plt.xlabel('Frequency')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im, cax=cax)
+    plt.show()
+
+
+
+#### helper functions ##########################
+
+def calc_feat_dim(window, max_freq):
+    return int(0.001 * window * max_freq) + 1
+
+
+# replace redundant letters
+def replacer(text):
+    replace_list = """ሐ ሑ ሒ ሓ ሔ ሕ ሖ ጸ ጹ ጺ ጻ ጼ ጽ ጾ ኰ ኲ ጿ ኸ""".split(" ")
+    ph = """ሀ ሁ ሂ ሀ ሄ ህ ሆ ፀ ፁ ፂ ፃ ፄ ፅ ፆ ኮ ኳ ፇ ኧ""".split(" ")
+    for l in range(len(replace_list)):
+       text = text.replace(replace_list[l], ph[l])
+    return text
+
+
+def text_to_int_sequence(text):
+    """ Convert text to an integer sequence """
+    int_sequence = []
+    for c in text:
+        if c == ' ':
+            ch = char_map[' ']
+        else:
+            # print("checking character " + c + " in map:")
+            # print(char_map)
+            ch = char_map[c]
+        int_sequence.append(ch)
+    return int_sequence
+
+
+def int_sequence_to_text(int_sequence):
+    """ Convert an integer sequence to text """
+    text = []
+    for c in int_sequence:
+        ch = index_map[c]
+        text.append(ch)
+    return text
+
+
+
+# Code adapted from https://martin-thoma.com/word-error-rate-calculation/
+def wer(r, h):
+    """
+    Calculation of WER with Levenshtein distance.
+
+    Works only for iterables up to 254 elements (uint8).
+    O(nm) time ans space complexity.
+
+    Parameters
+    ----------
+    r : list
+    h : list
+
+    Returns
+    -------
+    int
+
+    Examples
+    --------
+    >>> wer("who is there".split(), "is there".split())
+    1
+    >>> wer("who is there".split(), "".split())
+    3
+    >>> wer("".split(), "who is there".split())
+    3
+    """
+    # initialisation
+    import numpy
+    d = numpy.zeros((len(r)+1)*(len(h)+1), dtype=numpy.uint8)
+    d = d.reshape((len(r)+1, len(h)+1))
+    for i in range(len(r)+1):
+        for j in range(len(h)+1):
+            if i == 0:
+                d[0][j] = j
+            elif j == 0:
+                d[i][0] = i
+
+    # computation
+    for i in range(1, len(r)+1):
+        for j in range(1, len(h)+1):
+            if r[i-1] == h[j-1]:
+                d[i][j] = d[i-1][j-1]
+            else:
+                substitution = d[i-1][j-1] + 1
+                insertion    = d[i][j-1] + 1
+                deletion     = d[i-1][j] + 1
+                d[i][j] = min(substitution, insertion, deletion)
+
+    return d[len(r)][len(h)]
+
+
+"""
+Helper functions for plotting
+"""
+
+import pickle
+import matplotlib.pyplot as plt
+
+def plot_hist(p):
+    hist = pickle.load(open( "models/" + p + ".pickle", "rb"))
+    plt.plot(hist['loss'], label="train")
+    plt.plot(hist['val_loss'], label="valid")
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.legend()
+    plt.show
+
+
+def model_1(input_dim, units, activation, output_dim=29):
+    """ Build a recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add recurrent layer
+    simp_rnn = GRU(units, activation=activation,
+        return_sequences=True, implementation=2, name='rnn')(input_data)
+    # TODO: Add batch normalization 
+    bn_rnn = BatchNormalization()(simp_rnn)
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    #plot_model(model, to_file='../models/model_1.png')
+    return model
+
+
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+
+
+def add_ctc_loss(input_to_softmax):
+    the_labels = Input(name='the_labels', shape=(None,), dtype='float32')
+    input_lengths = Input(name='input_length', shape=(1,), dtype='int64')
+    label_lengths = Input(name='label_length', shape=(1,), dtype='int64')
+    output_lengths = Lambda(input_to_softmax.output_length)(input_lengths)
+    # CTC loss is implemented in a lambda layer
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')(
+        [input_to_softmax.output, the_labels, output_lengths, label_lengths])
+    model = Model(
+        inputs=[input_to_softmax.input, the_labels, input_lengths, label_lengths], 
+        outputs=loss_out)
+    return model
+
+
+def train(audio_gen,
+          input_to_softmax, 
+          model_name,
+          minibatch_size=20,
+          epochs=20,
+          verbose=1):    
+    # calculate steps_per_epoch
+    num_train_examples=len(audio_gen.train_audio_paths)
+    steps_per_epoch = num_train_examples//minibatch_size
+    # calculate validation_steps
+    num_valid_samples = len(audio_gen.valid_audio_paths) 
+    validation_steps = num_valid_samples//minibatch_size
+    
+    # add CTC loss to the NN specified in input_to_softmax
+    model = add_ctc_loss(input_to_softmax)
+
+    # CTC loss is implemented elsewhere, so use a dummy lambda function for the loss
+    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer='adam')
+
+    # make results/ directory, if necessary
+    if not os.path.exists('models'):
+        os.makedirs('models')
+
+    # add checkpointer
+    checkpointer = ModelCheckpoint(filepath='../models/'+model_name+'.h5', verbose=0)
+
+    # train the model
+    hist = model.fit(audio_gen.next_train(),epochs=epochs, batch_size=minibatch_size, verbose=2, 
+         validation_data=audio_gen.next_valid(), steps_per_epoch=steps_per_epoch, validation_steps=validation_steps,
+         use_multiprocessing=False)
+
+    # save model loss
+    with open('../models/'+model_name+'.pickle', 'wb') as f:
+        pickle.dump(hist.history, f)
+
+
+def predict(data_gen, index, partition, model, verbose=True):
+    """ Print a model's decoded predictions
+    Params:
+        data_gen: Data to run prediction on
+        index (int): Example to visualize
+        partition (str): Either 'train' or 'validation'
+        model (Model): The acoustic model
+    """
+    audio_path,data_point,transcr,prediction = predict_raw(data_gen, index, partition, model)
+    output_length = [model.output_length(data_point.shape[0])]
+    pred_ints = (K.eval(K.ctc_decode(
+                prediction, output_length, greedy=False)[0][0])+1).flatten().tolist()
+    predicted = ''.join(int_sequence_to_text(pred_ints)).replace("<SPACE>", " ")
+    wer_val = wer(transcr, predicted)
+    if verbose:
+        #display(Audio(audio_path, embed=True))
+        print('Truth: ' + transcr)
+        print('Predicted: ' + predicted)
+        print("wer: %d" % wer_val)
+    return wer_val
+
+
+def predict_raw(data_gen, index, partition, model):
+    """ Get a model's decoded predictions
+    Params:
+        data_gen: Data to run prediction on
+        index (int): Example to visualize
+        partition (str): Either 'train' or 'validation'
+        model (Model): The acoustic model
+    """
+
+    if partition == 'validation':
+        transcr = data_gen.valid_texts[index]
+        audio_path = data_gen.valid_audio_paths[index]
+        data_point = data_gen.normalize(data_gen.featurize(audio_path))
+    elif partition == 'train':
+        transcr = data_gen.train_texts[index]
+        audio_path = data_gen.train_audio_paths[index]
+        data_point = data_gen.normalize(data_gen.featurize(audio_path))
+    else:
+        raise Exception('Invalid partition!  Must be "train" or "validation"')
+        
+    prediction = model.predict(np.expand_dims(data_point, axis=0))
+    return (audio_path,data_point,transcr,prediction)
+
+
+
+def calculate_wer(model, model_name, data_gen, partition, length):
+    start = time.time()
+    def wer_single(i):
+        wer = predict(data_gen, i, partition, model, verbose=False)
+        if (i%100==0) and i>0:
+            print("processed %d in %d minutes" % (i, ((time.time() - start)/60)))
+        return wer
+    wer = list(map(lambda i: wer_single(i), range(1, length)))
+    print("Total time: %f minutes" % ((time.time() - start)/60))
+    filename = 'models/' + model_name + '_' + partition + '_wer.pickle'
+    with open(filename, 'wb') as handle:
+        pickle.dump(wer, handle)
+    return wer
