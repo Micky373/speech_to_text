fcb1619b468833486c138ad9dd94eaeb5408852b
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-06-06 17:27:33 -0700

    successfully modularized

--- scripts/acoustic_modeling.py
+++ scripts/acoustic_modeling.py
@@ -34,6 +34,57 @@ import os
 
 RNG_SEED = 123
 
+
+# The set of characters accepted in the transcription.
+supported = """
+ሀ ሁ ሂ ሄ ህ ሆ
+ለ ሉ ሊ ላ ሌ ል ሎ ሏ
+መ ሙ ሚ ማ ሜ ም ሞ ሟ
+ረ ሩ ሪ ራ ሬ ር ሮ ሯ
+ሰ ሱ ሲ ሳ ሴ ስ ሶ ሷ
+ሸ ሹ ሺ ሻ ሼ ሽ ሾ ሿ
+ቀ ቁ ቂ ቃ ቄ ቅ ቆ ቋ
+በ ቡ ቢ ባ ቤ ብ ቦ ቧ
+ቨ ቩ ቪ ቫ ቬ ቭ ቮ ቯ
+ተ ቱ ቲ ታ ቴ ት ቶ ቷ
+ቸ ቹ ቺ ቻ ቼ ች ቾ ቿ
+ኋ
+ነ ኑ ኒ ና ኔ ን ኖ ኗ
+ኘ ኙ ኚ ኛ ኜ ኝ ኞ ኟ
+አ ኡ ኢ ኤ እ ኦ
+ኧ
+ከ ኩ ኪ ካ ኬ ክ ኮ
+ኳ
+ወ ዉ ዊ ዋ ዌ ው ዎ
+ዘ ዙ ዚ ዛ ዜ ዝ ዞ ዟ
+ዠ ዡ ዢ ዣ ዤ ዥ ዦ ዧ
+የ ዩ ዪ ያ ዬ ይ ዮ
+ደ ዱ ዲ ዳ ዴ ድ ዶ ዷ
+ጀ ጁ ጂ ጃ ጄ ጅ ጆ ጇ
+ገ ጉ ጊ ጋ ጌ ግ ጐ ጓ ጔ
+ጠ ጡ ጢ ጣ ጤ ጥ ጦ ጧ
+ጨ ጩ ጪ ጫ ጬ ጭ ጮ ጯ
+ጰ ጱ ጲ ጳ ጴ ጵ ጶ ጷ
+ፀ ፁ ፂ ፃ ፄ ፅ ፆ ፇ
+ፈ ፉ ፊ ፋ ፌ ፍ ፎ ፏ
+ፐ ፑ ፒ ፓ ፔ ፕ ፖ
+""".split()
+
+char_map = {}
+char_map[""] = 0
+char_map[" "] = 1
+index = 2
+for c in supported:
+    char_map[c] = index
+    index += 1
+index_map = {v: k for k, v in char_map.items()}
+
+print(
+   # f"The vocabulary is: {char_map} "
+    #f"(size ={index_map})"
+)
+
+
 class AudioGenerator():
     def __init__(self,  train_meta, valid_meta, step=10, window=20, max_freq=8000, mfcc_dim=13,
         minibatch_size=20):
