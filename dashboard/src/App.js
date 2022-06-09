1e87ce357a2513480a26dacd34f164369712ad2f
abeselomg <abeselomgebrekidan12@gmail.com>
2022-06-09 22:39:02 +0300

    dashboared hosted

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,19 +1,7 @@
-import { Route, Routes } from "react-router";
 import "./App.css";
-import Sidebar from "./components/sidebar/Sidebar";
-import Home from "./pages/Home";
-import Upload from "./pages/Upload";
 
 function App() {
-  return (
-    <div className="h-screen flex">
-      <Sidebar className="fixed" />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} />
-      </Routes>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
